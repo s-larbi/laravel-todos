03d00cf7f89098165123ded219d0350ff50a565a
s-larbi <s.larbi@zeroandone.me>
2021-02-18 11:20:43 +0400

    add CDK Pipeline files

---
+++ cdk/app.ts
@@ -0,0 +1,29 @@
+
+#!/usr/bin/env node
+/*********************************
+ * AWS CDK script to provision the resources.
+ */
+
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { WebApp } from './webapp';
+import { Pipeline } from './pipeline';
+import { Cluster } from './cluster';
+
+class WebStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const cluster = new Cluster(this, 'Cluster');
+        const webapp = new WebApp(this, 'WebApp', {
+            cluster: cluster
+        });
+        const pipeline = new Pipeline(this, 'Pipeline', {
+            webapp: webapp
+        })
+    }
+}
+
+const app = new cdk.App();
+new WebStack(app, 'WebTestStack');
+app.synth();
